16b9d9e3fed95090abb51da5c54010aa38abdd3e
David Evander <davidevander@gmail.com>
2017-02-28 13:04:35 -0500

    initial HiLo

--- src/HiLo.java
+++ src/HiLo.java
@@ -6,21 +6,27 @@ public class HiLo {
 		//ready player 1
 		Scanner sc = new Scanner(System.in);
 		
-		//random integer creation
-		int number = (int)(Math.random()*100+1);
+		String restart = "";
 		
-		//setup game loop
-		int guess=0;
-		while(guess!=number){
-			System.out.println("Guess a number between 1-100");
-			guess=sc.nextInt();
-			if (guess<number)
-				System.out.println("You guessed too low, try again");
-			if (guess>number) 
-				System.out.println("You guessed too high, try again");
-		}
-		System.out.println("You guessed correctly!");
+		do{
+			//random integer creation
+			int number = (int)(Math.random()*100+1);
+			
+			//setup game loop
+			int guess=0;
+			while(guess!=number){
+				System.out.println("Guess a number between 1-100");
+				guess=sc.nextInt();
+				if (guess<number)
+					System.out.println("You guessed too low, try again");
+				if (guess>number) 
+					System.out.println("You guessed too high, try again");
+			}
+			System.out.println("You guessed correctly!  Play Again? (y/n)");
+			restart = sc.next();
+		} while (restart.equalsIgnoreCase("y"));
 		sc.close();
+		System.out.println("Alright, that's cool.  Bye.");
 		return;
 	}
 
