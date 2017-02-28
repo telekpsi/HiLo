0136981119fa30492fa58f8626f1ed11adde1434
David Evander <davidevander@gmail.com>
2017-02-28 13:56:55 -0500

    game completed, with exception-handling

--- src/HiLo.java
+++ src/HiLo.java
@@ -1,11 +1,12 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class HiLo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)throws InputMismatchException{
 		//ready player 1
 		Scanner sc = new Scanner(System.in);
-		
+		String sinEater = "I EAT YOUR ERRORS";
 		String restart = "";
 		
 		//setup outer game loop (never thought I'd see a use for a do-while...)
@@ -17,18 +18,28 @@ public class HiLo {
 			int guess=0;
 			while(guess!=number){
 				System.out.println("Guess a number between 1-100");
-				guess=sc.nextInt();
-				if (guess<number)
-					System.out.println("You guessed too low, try again");
-				if (guess>number) 
-					System.out.println("You guessed too high, try again");
+					try {
+						guess=sc.nextInt();
+						if (guess<1 ||guess>100)
+							System.out.println("STAY INSIDE THE ARBITRARY LINES!");
+						else if (guess<number)
+							System.out.println("You guessed too low, try again");
+						else if (guess>number) 
+							System.out.println("You guessed too high, try again");
+					} catch (InputMismatchException e) {
+						System.out.println("I need a number here, buddy");
+						sinEater=sc.next();
+						continue;
+					}
+				
 			}
 			System.out.println("You guessed correctly!  Play Again? (y/n)");
 			restart = sc.next();
 		} while (restart.equalsIgnoreCase("y"));
 		
+		restart=sinEater;
 		sc.close();
-		System.out.println("Alright, that's cool.  Bye.");
+		System.out.println("Whatever.  Bye.");
 		return;
 	}
 
