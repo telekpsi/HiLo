1838e2ac17bca2673d28762339d9908f16602046
David Evander <davidevander@gmail.com>
2017-02-28 12:57:02 -0500

    initial HiLo

--- src/HiLo.java
+++ src/HiLo.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
 
 public class HiLo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//ready player 1
+		Scanner sc = new Scanner(System.in);
+		
+		//random integer creation
+		int number = (int)(Math.random()*100+1);
+		
+		//setup game loop
+		int guess=0;
+		while(guess!=number){
+			System.out.println("Guess a number between 1-100");
+			guess=sc.nextInt();
+			if (guess<number)
+				System.out.println("You guessed too low, try again");
+			if (guess>number) 
+				System.out.println("You guessed too high, try again");
+		}
+		System.out.println("You guessed correctly!");
+		sc.close();
+		return;
 	}
 
 }
